3c3b4f2250d56f8f8e36810a4bf3b5725f8844f5
Jan Nedbal <janedbal@gmail.com>
2022-06-14 12:17:12 +0200

    UnusedExceptionRule: improve test

--- tests/Rule/data/UnusedExceptionRule/code.php
+++ tests/Rule/data/UnusedExceptionRule/code.php
@@ -8,6 +8,11 @@ class ExampleClass
         $this->getExceptionAtRuntime(); // error: Method $this->getExceptionAtRuntime() returns exception that was not used in any way.
         $this->getException(); // error: Method $this->getException() returns exception that was not used in any way.
         new \Exception(); // error: Exception new \Exception() was not used in any way.
+
+        $this->okUsage1();
+        $this->okUsage2();
+        $this->okUsage3();
+        $this->okUsage4(new \LogicException());
     }
 
     public function okUsage1(): void
@@ -23,6 +28,11 @@ class ExampleClass
     public function okUsage3(): void
     {
         throw $this->getExceptionAtRuntime();
+    }
+
+    public function okUsage4(Throwable $throwable): void
+    {
+        $this->okUsage4($throwable);
     }
 
     public function getExceptionAtRuntime(): \RuntimeException
