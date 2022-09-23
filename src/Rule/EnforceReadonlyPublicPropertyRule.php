bf3b86be56441f4e1829a414b797fd10ce99d22d
Jan Nedbal <janedbal@gmail.com>
2022-09-23 15:51:47 +0200

    EnforceReadonlyPublicPropertyRule (#31)
    * EnforceReadonlyPublicPropertyRule
    
    * readme

---
+++ src/Rule/EnforceReadonlyPublicPropertyRule.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\PHPStan\Rule;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\ClassPropertyNode;
+use PHPStan\Rules\Rule;
+
+/**
+ * @implements Rule<ClassPropertyNode>
+ */
+class EnforceReadonlyPublicPropertyRule implements Rule
+{
+
+    public function getNodeType(): string
+    {
+        return ClassPropertyNode::class;
+    }
+
+    /**
+     * @param ClassPropertyNode $node
+     * @return string[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node->isPublic() || $node->isReadOnly()) {
+            return [];
+        }
+
+        $classReflection = $scope->getClassReflection();
+
+        if ($classReflection === null) {
+            return [];
+        }
+
+        if (($classReflection->getNativeReflection()->getModifiers() & 65_536) !== 0) { // readonly class, since PHP 8.2
+            return [];
+        }
+
+        return ["Public property `{$node->getName()}` not marked as readonly."];
+    }
+
+}
---
+++ tests/Rule/data/EnforceReadonlyPublicPropertyRule/code.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace EnforceReadonlyPublicPropertyRule;
+
+trait MyTrait {
+
+    public ?string $public; // error: Public property `public` not marked as readonly.
+
+    public readonly string $publicReadonly;
+
+    protected string $protected;
+
+    private string $private;
+
+}
+
+class MyClass {
+
+    use MyTrait;
+
+    public ?int $foo; // error: Public property `foo` not marked as readonly.
+
+    public readonly int $bar;
+
+    protected int $baz;
+
+    private int $bag;
+
+}
+
