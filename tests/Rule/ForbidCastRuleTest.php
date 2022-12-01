30abc1e5de8c3587ffd76ca0dfd162d6fcb6f923
Jan Nedbal <janedbal@gmail.com>
2022-12-01 15:19:06 +0100

    ForbidCastRule

---
+++ tests/Rule/ForbidCastRuleTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\PHPStan\Rule;
+
+use PHPStan\Rules\Rule;
+use ShipMonk\PHPStan\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<ForbidCastRule>
+ */
+class ForbidCastRuleTest extends RuleTestCase
+{
+
+    protected function getRule(): Rule
+    {
+        return new ForbidCastRule();
+    }
+
+    public function testClass(): void
+    {
+        $this->analyseFile(__DIR__ . '/data/ForbidCastRule/code.php');
+    }
+
+}
