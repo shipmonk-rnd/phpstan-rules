158acc9e1fe1746f920beeb330943d7f07e9f2a1
Jan Nedbal <janedbal@gmail.com>
2023-03-31 16:24:17 +0200

    simplify

--- src/Rule/EnforceEnumMatchRule.php
+++ src/Rule/EnforceEnumMatchRule.php
@@ -3,15 +3,11 @@
 namespace ShipMonk\PHPStan\Rule;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\BinaryOp\Identical;
-use PhpParser\Node\Expr\BinaryOp\NotIdentical;
 use PhpParser\Node\Stmt\ElseIf_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Parser\LastConditionVisitor;
 use PHPStan\Rules\Rule;
-use function array_unique;
-use function count;
 
 /**
  * @implements Rule<ElseIf_>
@@ -30,45 +26,29 @@ class EnforceEnumMatchRule implements Rule
      */
     public function processNode(Node $elseif, Scope $scope): array
     {
-        if ($elseif->cond->getAttribute(LastConditionVisitor::ATTRIBUTE_NAME) === true) { // @phpstan-ignore-line ignore BC promise
-            if (!$this->comparesEnums($elseif->cond, $scope)) {
-                return [];
-            }
-
-            $conditionType = $scope->getType($elseif->cond);
-
-            if ($conditionType->isTrue()->yes()) {
-                return ['This else-if chain looks like exhaustive enum check. Rewrite it to match construct to ensure that error is raised when new enum case is added.'];
-            }
+        if (
+            $elseif->cond->getAttribute(LastConditionVisitor::ATTRIBUTE_NAME) === true // @phpstan-ignore-line ignore BC promise
+            && $elseif->cond instanceof Identical
+            && $this->isAlwaysTrueEnumComparison($elseif->cond, $scope)
+        ) {
+            return ['This else-if chain looks like exhaustive enum check. Rewrite it to match construct to ensure that error is raised when new enum case is added.'];
         }
 
         return [];
     }
 
-    private function comparesEnums(Expr $condition, Scope $scope): bool
+    private function isAlwaysTrueEnumComparison(Identical $condition, Scope $scope): bool
     {
-        if (!$condition instanceof Identical && !$condition instanceof NotIdentical) {
+        $conditionType = $scope->getType($condition);
+
+        if (!$conditionType->isTrue()->yes()) {
             return false;
         }
 
         $leftType = $scope->getType($condition->left);
         $rightType = $scope->getType($condition->right);
 
-        if (!$leftType->isEnum()->yes() && !$rightType->isEnum()->yes()) {
-            return false;
-        }
-
-        $enumClassNames = [];
-
-        foreach ($leftType->getEnumCases() as $enumCase) {
-            $enumClassNames[] = $enumCase->getClassName();
-        }
-
-        foreach ($rightType->getEnumCases() as $enumCase) {
-            $enumClassNames[] = $enumCase->getClassName();
-        }
-
-        return count(array_unique($enumClassNames)) === 1;
+        return $leftType->isEnum()->yes() && $rightType->isEnum()->yes();
     }
 
 }
