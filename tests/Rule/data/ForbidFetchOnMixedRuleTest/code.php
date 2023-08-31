092b785109669f10bf14a3fe33b1638d1873a8f1
Jan Nedbal <janedbal@gmail.com>
2023-08-31 15:56:12 +0200

    OnMixedRules: report even static fetch/call (#143)

--- tests/Rule/data/ForbidFetchOnMixedRuleTest/code.php
+++ tests/Rule/data/ForbidFetchOnMixedRuleTest/code.php
@@ -4,9 +4,19 @@ namespace ForbidFetchOnMixedRuleTest;
 
 use ReflectionClass;
 
+class Foo {
+    public ?int $property = null;
+    public static ?int $staticProperty = null;
+}
+
 $fn = function (mixed $mixed, $unknown, array $array, ReflectionClass $reflection) {
+    (new Foo)->property;
+    Foo::$staticProperty;
+
     $mixed->fetch1; // error: Property fetch ->fetch1 is prohibited on unknown type ($mixed)
+    $mixed::$fetch1; // error: Property fetch ::$fetch1 is prohibited on unknown type ($mixed)
     $unknown->fetch2; // error: Property fetch ->fetch2 is prohibited on unknown type ($unknown)
+    $unknown::$fetch2; // error: Property fetch ::$fetch2 is prohibited on unknown type ($unknown)
     $array[0]->fetch3; // error: Property fetch ->fetch3 is prohibited on unknown type ($array[0])
     $reflection->newInstance()->fetch4;
 };
--- tests/Rule/data/ForbidMethodCallOnMixedRule/code.php
+++ tests/Rule/data/ForbidMethodCallOnMixedRule/code.php
@@ -4,9 +4,25 @@ namespace ForbidMethodCallOnMixedRule;
 
 use ReflectionClass;
 
+class Foo {
+    public function method() {}
+    public static function staticMethod() {}
+}
+
 $fn = function (mixed $mixed, $unknown, array $array, ReflectionClass $reflection) {
+
+    $foo = new Foo();
+    $foo->method();
+    $foo?->method();
+    Foo::staticMethod();
+    $foo::staticMethod();
+
     $mixed->call1(); // error: Method call ->call1() is prohibited on unknown type ($mixed)
+    $mixed?->call1(); // error: Method call ->call1() is prohibited on unknown type ($mixed)
+    $mixed::call1(); // error: Method call ::call1() is prohibited on unknown type ($mixed)
     $unknown->call2(); // error: Method call ->call2() is prohibited on unknown type ($unknown)
+    $unknown?->call2(); // error: Method call ->call2() is prohibited on unknown type ($unknown)
+    $unknown::call2(); // error: Method call ::call2() is prohibited on unknown type ($unknown)
     $array[0]->call3(); // error: Method call ->call3() is prohibited on unknown type ($array[0])
     $reflection->newInstance()->call4();
 };
