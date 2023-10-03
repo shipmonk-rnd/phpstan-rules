64442f907e682ee4f838e4ee2e5160fa36f8e842
Jan Nedbal <janedbal@gmail.com>
2023-10-03 12:38:17 +0200

    ForbidMethodCallOnMixedRule: use ?-> in error message for nullsafe

--- tests/Rule/data/ForbidMethodCallOnMixedRule/code.php
+++ tests/Rule/data/ForbidMethodCallOnMixedRule/code.php
@@ -33,10 +33,10 @@ $fn = function (mixed $mixed, $unknown, array $array, string $string, ?Foo $fooO
     $string::staticMethod(); // error: Method call ::staticMethod() is prohibited on unknown type ($string)
 
     $mixed->call1(); // error: Method call ->call1() is prohibited on unknown type ($mixed)
-    $mixed?->call1(); // error: Method call ->call1() is prohibited on unknown type ($mixed)
+    $mixed?->call1(); // error: Method call ?->call1() is prohibited on unknown type ($mixed)
     $mixed::call1(); // error: Method call ::call1() is prohibited on unknown type ($mixed)
     $unknown->call2(); // error: Method call ->call2() is prohibited on unknown type ($unknown)
-    $unknown?->call2(); // error: Method call ->call2() is prohibited on unknown type ($unknown)
+    $unknown?->call2(); // error: Method call ?->call2() is prohibited on unknown type ($unknown)
     $unknown::call2(); // error: Method call ::call2() is prohibited on unknown type ($unknown)
     $array[0]->call3(); // error: Method call ->call3() is prohibited on unknown type ($array[0])
 
