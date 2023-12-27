c4ba7f97f6c2ec40ffe2404a9dfcca2c275cbdf2
Jan Nedbal <janedbal@gmail.com>
2023-12-27 09:59:21 +0100

    Always use getKeepVoidType

--- src/Rule/ForbidUnusedMatchResultRule.php
+++ src/Rule/ForbidUnusedMatchResultRule.php
@@ -10,11 +10,9 @@ use PHPStan\Rules\IdentifierRuleError;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
 use PHPStan\Type\NeverType;
-use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 use PHPStan\Type\VerbosityLevel;
 use ShipMonk\PHPStan\Visitor\UnusedMatchVisitor;
-use function method_exists;
 
 /**
  * @implements Rule<Match_>
@@ -36,10 +34,7 @@ class ForbidUnusedMatchResultRule implements Rule
         $returnedTypes = [];
 
         foreach ($node->arms as $arm) {
-            /** @var Type $armType */
-            $armType = method_exists($scope, 'getKeepVoidType') // Needed since https://github.com/phpstan/phpstan/releases/tag/1.10.49, can be dropped once we bump PHPStan version gte that
-                ? $scope->getKeepVoidType($arm->body)
-                : $scope->getType($arm->body);
+            $armType = $scope->getKeepVoidType($arm->body);
 
             if (!$armType->isVoid()->yes() && !$armType instanceof NeverType && !$arm->body instanceof Assign) {
                 $returnedTypes[] = $armType;
