13d4b50a22714fdf82ca50ad7a779df925b001c4
Jan Nedbal <janedbal@gmail.com>
2025-12-18 16:40:11 +0100

    No PHPStan ignores, remove useless code

--- tests/Rule/ForbidCastRuleTest.php
+++ tests/Rule/ForbidCastRuleTest.php
@@ -2,10 +2,8 @@
 
 namespace ShipMonk\PHPStan\Rule;
 
-use PHPStan\Php\PhpVersion;
 use PHPStan\Rules\Rule;
 use ShipMonk\PHPStan\RuleTestCase;
-use const PHP_VERSION_ID;
 
 /**
  * @extends RuleTestCase<ForbidCastRule>
@@ -25,17 +23,7 @@ class ForbidCastRuleTest extends RuleTestCase
 
     public function testClassPhp85(): void
     {
-        if (PHP_VERSION_ID < 80_500) {
-            self::markTestSkipped('Requires PHP 8.5');
-        }
-
-        $this->phpVersion = $this->createPhpVersion(80_500);
         $this->analyseFile(__DIR__ . '/data/ForbidCastRule/code-85.php');
-    }
-
-    private function createPhpVersion(int $version): PhpVersion
-    {
-        return new PhpVersion($version);
     }
 
 }
